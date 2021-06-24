9629b4dcb85868aa3ef042424841e11a3cbf9913
Rainc1116 <52766106+Rainc1116@users.noreply.github.com>
2021-06-24 13:38:51 +0800

    Update AdminDoctorController.java
    修改错误

--- litemall-admin-api/src/main/java/org/linlinjava/litemall/admin/web/AdminDoctorController.java
+++ litemall-admin-api/src/main/java/org/linlinjava/litemall/admin/web/AdminDoctorController.java
@@ -7,8 +7,8 @@ import org.linlinjava.litemall.admin.annotation.RequiresPermissionsDesc;
 import org.linlinjava.litemall.core.util.ResponseUtil;
 import org.linlinjava.litemall.core.validator.Order;
 import org.linlinjava.litemall.core.validator.Sort;
-import org.linlinjava.litemall.db.domain.LitemallDoctor;
-import org.linlinjava.litemall.db.service.LitemallDoctorService;
+import org.linlinjava.litemall.db.domain.LitemallDoctors;
+import org.linlinjava.litemall.db.service.LitemallDoctorsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,11 +25,11 @@ import javax.validation.constraints.NotNull;
 @RestController
 @RequestMapping("/admin/doctor")
 @Validated
-public class AdminDcotorController {
-    private final Log logger = LogFactory.getLog(AdminDcotorController.class);
+public class AdminDoctorController {
+    private final Log logger = LogFactory.getLog(AdminDoctorController.class);
 
     @Autowired
-    private LitemallDoctorService DoctorService;
+    private LitemallDoctorsService DoctorService;
 
     @RequiresPermissions("admin:doctor:list")
     @RequiresPermissionsDesc(menu = {"用户管理", "医生管理"}, button = "查询")
@@ -39,20 +39,20 @@ public class AdminDcotorController {
                        @RequestParam(defaultValue = "10") Integer limit,
                        @Sort @RequestParam(defaultValue = "add_time") String sort,
                        @Order @RequestParam(defaultValue = "desc") String order) {
-        List<LitemallDoctor> doctorList = doctorService.querySelective(doctorname, mobile, page, limit, sort, order);
+        List<LitemallDoctors> doctorList = DoctorService.querySelective(doctorname, mobile, page, limit, sort, order);
         return ResponseUtil.okList(doctorList);
     }
     @RequiresPermissions("admin:doctor:list")
     @RequiresPermissionsDesc(menu = {"用户管理", "医生管理"}, button = "详情")
     @GetMapping("/detail")
     public Object doctorDetail(@NotNull Integer id) {
-    	LitemallDoctor doctor=doctorService.findById(id);
+    	LitemallDoctors doctor=DoctorService.findById(id);
         return ResponseUtil.ok(doctor);
     }
     @RequiresPermissions("admin:doctor:list")
     @RequiresPermissionsDesc(menu = {"用户管理", "医生管理"}, button = "编辑")
     @PostMapping("/update")
-    public Object doctorUpdate(@RequestBody LitemallDoctor doctor) {
-        return ResponseUtil.ok(doctorService.updateById(doctor));
+    public Object doctorUpdate(@RequestBody LitemallDoctors doctor) {
+        return ResponseUtil.ok(DoctorService.updateById(doctor));
     }
 }
